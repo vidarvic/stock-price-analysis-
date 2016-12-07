bd64b849925e584ede29620a8e36771516c47ac0
cpm.chuck <cpm.chuck@gmail.com>
2016-12-07 21:27:28 +0800

    Features and Targets
    Reverse the order of time and create the features and targets.

--- crawler_from_yahoo_finance.py
+++ crawler_from_yahoo_finance.py
@@ -7,30 +7,42 @@ Chi-Lun Mai
 from yahoo_finance import Share
 
 yahoo = Share('YHOO')
-data = yahoo.get_historical('2015-01-01', '2015-12-31')
+datatemp = yahoo.get_historical('2015-01-01', '2015-12-31')
+data = []
+
+# Reverse the order of time.
+for i in range(len(datatemp)):
+	data.append(datatemp[len(datatemp)-i-1])
 
 headers = ['Symbol','Date','High','Low','Close','Open','Volume','Adj_Close']
 
 # Output the csv file.
 import csv
 with open('yahoo_2015.csv','w') as f:
-    f_csv = csv.DictWriter(f, headers)
-    f_csv.writeheader()
-    f_csv.writerows(data)
+	f_csv = csv.DictWriter(f, headers)
+	f_csv.writeheader()
+	f_csv.writerows(data)
 
 # Output the json file.
 import json
 with open('yahoo_2015.json', 'w') as f:
-    json.dump(data, f)
+	json.dump(data, f)
 
-# Store data in numpy-array form.
-# Delete the non-number data(Volume, Symbol, Date).
+'''
+Store feature in numpy-array form.
+'''
+
+# Data feature is T
 import numpy as np
-yahoo_2015 = np.empty((0,5))
-for i in range(200):
-    del data[i]['Volume']
-    del data[i]['Symbol']
-    del data[i]['Date']
-	yahoo_2015 = np.vstack((yahoo_2015, np.array(data[i].values())))
+yahoo_target = np.empty((0,1))
+for i in range(len(data)-1):
+	yahoo_target = np.vstack((yahoo_target, np.array(data[i+1]['Close'])))
 
-print yahoo_2015
+# Delete the non-number data(Symbol, Date).
+# Data feature is T-1.
+yahoo_feature_t1 = np.empty((0,6))
+for i in range(len(data)-1):
+	del data[i]['Symbol']
+	del data[i]['Date']
+	yahoo_feature_t1 = np.vstack((yahoo_feature_t1, np.array(data[i].values())))
+
