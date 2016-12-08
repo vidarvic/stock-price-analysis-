7877d8ee9c1bb4a30086815a96816e28458767e8
cpm.chuck <cpm.chuck@gmail.com>
2016-12-09 00:33:20 +0800

    LR test
    Complete the LR-test, but still need to interpret the meaning of
    outcome.

--- crawler_from_yahoo_finance.py
+++ crawler_from_yahoo_finance.py
@@ -1,7 +1,7 @@
 '''
 Dec 5, 2016
 Crawler from YAHOO finance
-Chi-Lun Mai
+CPMAI
 '''
 
 from yahoo_finance import Share
@@ -36,7 +36,8 @@ Store feature in numpy-array form.
 import numpy as np
 yahoo_target = np.empty((0,1))
 for i in range(len(data)-1):
-	yahoo_target = np.vstack((yahoo_target, np.array(data[i+1]['Close'])))
+	yahoo_target = np.append(yahoo_target, float(data[i+1]['Close']))
+	#yahoo_target = np.vstack((yahoo_target, np.array(data[i+1]['Close'])))
 
 # Delete the non-number data(Symbol, Date).
 # Data feature is T-1.
@@ -46,3 +47,37 @@ for i in range(len(data)-1):
 	del data[i]['Date']
 	yahoo_feature_t1 = np.vstack((yahoo_feature_t1, np.array(data[i].values())))
 
+# Convert the type of data.
+yahoo_target = yahoo_target.astype(float)
+yahoo_feature_t1 = yahoo_feature_t1.astype(float)	
+'''
+Machine Learning
+'''
+
+#%matplotlib inline
+from sklearn import datasets
+from sklearn.cross_validation import cross_val_predict
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+
+lr = linear_model.LinearRegression()
+y = yahoo_target
+predicted = cross_val_predict(lr, yahoo_feature_t1, y, cv=10)
+
+from sklearn.externals import joblib
+joblib.dump(lr,"./lr_machine.pkl")
+
+
+lr=joblib.load("./lr_machine.pkl")
+
+lr.fit(yahoo_feature_t1, y)
+predict_y=lr.predict(yahoo_feature_t1[2])
+
+
+plt.scatter(predicted, y, s=2)
+plt.plot(predict_y, predict_y, 'ro')
+
+plt.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=2)
+
+plt.xlabel('Predicted')
+plt.ylabel('Measured')
